398876ca69d9a0faea22a6a10a9dded46476c0cf
PStrib <peter.strib@gmail.com>
2021-11-09 12:19:55 +0000

    Removes surplus buttons from MainApp.CS design and adds "Add Client" Button

--- CreatingADatabase/GUI/MainApp.Designer.cs
+++ CreatingADatabase/GUI/MainApp.Designer.cs
@@ -30,10 +30,7 @@ namespace CreatingADatabase.GUI
         private void InitializeComponent()
         {
             this.WelcomeBanner = new System.Windows.Forms.Label();
-            this.btnSearchTool = new System.Windows.Forms.Button();
-            this.btnEditProject = new System.Windows.Forms.Button();
-            this.btnAddProject = new System.Windows.Forms.Button();
-            this.btnInnerJoin = new System.Windows.Forms.Button();
+            this.btnAddClient = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // WelcomeBanner
@@ -43,71 +40,31 @@ namespace CreatingADatabase.GUI
             this.WelcomeBanner.Location = new System.Drawing.Point(11, 7);
             this.WelcomeBanner.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.WelcomeBanner.Name = "WelcomeBanner";
-            this.WelcomeBanner.Size = new System.Drawing.Size(412, 30);
+            this.WelcomeBanner.Size = new System.Drawing.Size(345, 30);
             this.WelcomeBanner.TabIndex = 0;
-            this.WelcomeBanner.Text = "Project Management Application";
+            this.WelcomeBanner.Text = "Simpson\'s Department Store";
             // 
-            // btnSearchTool
+            // btnAddClient
             // 
-            this.btnSearchTool.Font = new System.Drawing.Font("Gazelle", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearchTool.Location = new System.Drawing.Point(95, 43);
-            this.btnSearchTool.Margin = new System.Windows.Forms.Padding(2);
-            this.btnSearchTool.Name = "btnSearchTool";
-            this.btnSearchTool.Size = new System.Drawing.Size(140, 35);
-            this.btnSearchTool.TabIndex = 1;
-            this.btnSearchTool.Text = "Search Tool";
-            this.btnSearchTool.UseVisualStyleBackColor = true;
-            this.btnSearchTool.Click += new System.EventHandler(this.btnSearchTool_Click);
-            // 
-            // btnEditProject
-            // 
-            this.btnEditProject.Font = new System.Drawing.Font("Gazelle", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditProject.Location = new System.Drawing.Point(95, 82);
-            this.btnEditProject.Margin = new System.Windows.Forms.Padding(2);
-            this.btnEditProject.Name = "btnEditProject";
-            this.btnEditProject.Size = new System.Drawing.Size(140, 35);
-            this.btnEditProject.TabIndex = 2;
-            this.btnEditProject.Text = "Edit Project";
-            this.btnEditProject.UseVisualStyleBackColor = true;
-            this.btnEditProject.Click += new System.EventHandler(this.btnEditProject_Click);
-            // 
-            // btnAddProject
-            // 
-            this.btnAddProject.Font = new System.Drawing.Font("Gazelle", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddProject.Location = new System.Drawing.Point(95, 121);
-            this.btnAddProject.Margin = new System.Windows.Forms.Padding(2);
-            this.btnAddProject.Name = "btnAddProject";
-            this.btnAddProject.Size = new System.Drawing.Size(140, 35);
-            this.btnAddProject.TabIndex = 3;
-            this.btnAddProject.Text = "Add Project";
-            this.btnAddProject.UseVisualStyleBackColor = true;
-            this.btnAddProject.Click += new System.EventHandler(this.btnAddProject_Click);
-            // 
-            // btnInnerJoin
-            // 
-            this.btnInnerJoin.Font = new System.Drawing.Font("Gazelle", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInnerJoin.Location = new System.Drawing.Point(95, 160);
-            this.btnInnerJoin.Margin = new System.Windows.Forms.Padding(2);
-            this.btnInnerJoin.Name = "btnInnerJoin";
-            this.btnInnerJoin.Size = new System.Drawing.Size(140, 35);
-            this.btnInnerJoin.TabIndex = 4;
-            this.btnInnerJoin.Text = "Inner Join";
-            this.btnInnerJoin.UseVisualStyleBackColor = true;
-            this.btnInnerJoin.Click += new System.EventHandler(this.btnInnerJoin_Click);
+            this.btnAddClient.Font = new System.Drawing.Font("Gazelle", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddClient.Location = new System.Drawing.Point(103, 39);
+            this.btnAddClient.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAddClient.Name = "btnAddClient";
+            this.btnAddClient.Size = new System.Drawing.Size(140, 35);
+            this.btnAddClient.TabIndex = 5;
+            this.btnAddClient.Text = "Add Client";
+            this.btnAddClient.UseVisualStyleBackColor = true;
             // 
             // MainApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 232);
-            this.Controls.Add(this.btnInnerJoin);
-            this.Controls.Add(this.btnAddProject);
-            this.Controls.Add(this.btnEditProject);
-            this.Controls.Add(this.btnSearchTool);
+            this.ClientSize = new System.Drawing.Size(359, 244);
+            this.Controls.Add(this.btnAddClient);
             this.Controls.Add(this.WelcomeBanner);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainApp";
-            this.Text = "MainApp";
+            this.Text = "Simpson\'s Department Store";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,9 +73,6 @@ namespace CreatingADatabase.GUI
         #endregion
 
         private System.Windows.Forms.Label WelcomeBanner;
-        private System.Windows.Forms.Button btnSearchTool;
-        private System.Windows.Forms.Button btnEditProject;
-        private System.Windows.Forms.Button btnAddProject;
-        private System.Windows.Forms.Button btnInnerJoin;
+        private System.Windows.Forms.Button btnAddClient;
     }
 }
