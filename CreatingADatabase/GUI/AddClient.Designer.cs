382e8af5564c3995435b8cc67e8571ebc1f7ad02
PStrib <peter.strib@gmail.com>
2021-11-10 09:32:29 +0000

    Adds client.cs class and fills in the getters and setters

---
+++ CreatingADatabase/GUI/AddClient.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace CreatingADatabase.GUI
+{
+    partial class AddClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContactForename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContactSurname = new System.Windows.Forms.TextBox();
+            this.txtBusinessName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHouseNo = new System.Windows.Forms.TextBox();
+            this.txtStreetName = new System.Windows.Forms.TextBox();
+            this.txtTownName = new System.Windows.Forms.TextBox();
+            this.txtPostCode = new System.Windows.Forms.TextBox();
+            this.txtCounty = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.txtContactEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnAddClient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContactForename
+            // 
+            this.txtContactForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContactForename.Location = new System.Drawing.Point(152, 74);
+            this.txtContactForename.Name = "txtContactForename";
+            this.txtContactForename.Size = new System.Drawing.Size(115, 22);
+            this.txtContactForename.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Fipps", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-5, -9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 81);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Add a Client";
+            // 
+            // txtContactSurname
+            // 
+            this.txtContactSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContactSurname.Location = new System.Drawing.Point(152, 102);
+            this.txtContactSurname.Name = "txtContactSurname";
+            this.txtContactSurname.Size = new System.Drawing.Size(115, 22);
+            this.txtContactSurname.TabIndex = 2;
+            // 
+            // txtBusinessName
+            // 
+            this.txtBusinessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBusinessName.Location = new System.Drawing.Point(152, 210);
+            this.txtBusinessName.Name = "txtBusinessName";
+            this.txtBusinessName.Size = new System.Drawing.Size(115, 22);
+            this.txtBusinessName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contact Forename:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Contact Surname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Business Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(87, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "House No:";
+            // 
+            // txtHouseNo
+            // 
+            this.txtHouseNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHouseNo.Location = new System.Drawing.Point(152, 238);
+            this.txtHouseNo.Name = "txtHouseNo";
+            this.txtHouseNo.Size = new System.Drawing.Size(115, 22);
+            this.txtHouseNo.TabIndex = 8;
+            // 
+            // txtStreetName
+            // 
+            this.txtStreetName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStreetName.Location = new System.Drawing.Point(152, 266);
+            this.txtStreetName.Name = "txtStreetName";
+            this.txtStreetName.Size = new System.Drawing.Size(115, 22);
+            this.txtStreetName.TabIndex = 10;
+            // 
+            // txtTownName
+            // 
+            this.txtTownName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTownName.Location = new System.Drawing.Point(152, 294);
+            this.txtTownName.Name = "txtTownName";
+            this.txtTownName.Size = new System.Drawing.Size(115, 22);
+            this.txtTownName.TabIndex = 11;
+            // 
+            // txtPostCode
+            // 
+            this.txtPostCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPostCode.Location = new System.Drawing.Point(152, 322);
+            this.txtPostCode.Name = "txtPostCode";
+            this.txtPostCode.Size = new System.Drawing.Size(115, 22);
+            this.txtPostCode.TabIndex = 12;
+            // 
+            // txtCounty
+            // 
+            this.txtCounty.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCounty.Location = new System.Drawing.Point(152, 350);
+            this.txtCounty.Name = "txtCounty";
+            this.txtCounty.Size = new System.Drawing.Size(115, 22);
+            this.txtCounty.TabIndex = 13;
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContactNo.Location = new System.Drawing.Point(152, 130);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(115, 22);
+            this.txtContactNo.TabIndex = 14;
+            // 
+            // txtContactEmail
+            // 
+            this.txtContactEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContactEmail.Location = new System.Drawing.Point(152, 158);
+            this.txtContactEmail.Name = "txtContactEmail";
+            this.txtContactEmail.Size = new System.Drawing.Size(115, 22);
+            this.txtContactEmail.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 15);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Contact Phone Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(63, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Contact Email:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(108, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 15);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Street:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(111, 298);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 15);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Town:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(88, 326);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 15);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "PostCode:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(101, 354);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(48, 15);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "County:";
+            // 
+            // btnAddClient
+            // 
+            this.btnAddClient.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddClient.Location = new System.Drawing.Point(31, 378);
+            this.btnAddClient.Name = "btnAddClient";
+            this.btnAddClient.Size = new System.Drawing.Size(221, 45);
+            this.btnAddClient.TabIndex = 22;
+            this.btnAddClient.Text = "Add";
+            this.btnAddClient.UseVisualStyleBackColor = true;
+            this.btnAddClient.Click += new System.EventHandler(this.btnAddClient_Click);
+            // 
+            // AddClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 451);
+            this.Controls.Add(this.btnAddClient);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtContactEmail);
+            this.Controls.Add(this.txtContactNo);
+            this.Controls.Add(this.txtCounty);
+            this.Controls.Add(this.txtPostCode);
+            this.Controls.Add(this.txtTownName);
+            this.Controls.Add(this.txtStreetName);
+            this.Controls.Add(this.txtHouseNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBusinessName);
+            this.Controls.Add(this.txtContactSurname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContactForename);
+            this.Name = "AddClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContactForename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContactSurname;
+        private System.Windows.Forms.TextBox txtBusinessName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHouseNo;
+        private System.Windows.Forms.TextBox txtStreetName;
+        private System.Windows.Forms.TextBox txtTownName;
+        private System.Windows.Forms.TextBox txtPostCode;
+        private System.Windows.Forms.TextBox txtCounty;
+        private System.Windows.Forms.TextBox txtContactNo;
+        private System.Windows.Forms.TextBox txtContactEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnAddClient;
+    }
+}
---
+++ CreatingADatabase/Objects/Client.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingADatabase.Objects
+{
+    class Client
+    {
+        private int clientID;
+        private string contactForename;
+        private string contactSurname;
+        private string businessName;
+        private int houseNo;
+        private string streetName;
+        private string townName;
+        private string postCode;
+        private string county;
+        private string contactPhoneNo;
+        private string contactEmail;
+
+        public Client()
+        { }
+
+        public Client(int clientID, string contactForename, string contactSurname, string businessName, int houseNo, string streetName, string townName, string postCode, string county, string contactPhoneNo, string contactEmail)
+        {
+            clientID = ClientID;
+            contactForename = ContactForename;
+            contactSurname = ContactSurname;
+            businessName = BusinessName;
+            houseNo = HouseNo;
+            streetName = StreetName;
+            townName = TownName;
+            postCode = PostCode;
+            county = County;
+            contactPhoneNo = ContactPhoneNo;
+            contactEmail = ContactEmail;
+        }
+
+        public int ClientID
+        {
+            get
+            {
+                return clientID;
+            }
+
+            set
+            {
+                clientID = value;
+            }
+        }
+
+        public string ContactForename
+        {
+            get
+            {
+                return contactForename;
+            }
+            set
+            {
+                contactForename = value;
+            }
+        }
+        public string ContactSurname
+        {
+            get
+            {
+                return contactSurname;
+            }
+            set
+            {
+                contactSurname = value;
+            }
+        }
+        public string BusinessName
+        {
+            get
+            {
+                return businessName;
+            }
+            set
+            {
+                businessName = value;
+            }
+        }
+        public int HouseNo
+        {
+            get
+            {
+                return houseNo;
+            }
+            set
+            {
+                houseNo = value;
+            }
+        }
+
+        public string StreetName
+        {
+            get
+            {
+                return streetName;
+            }
+            set
+            {
+                streetName = value;
+            }
+        }
+
+        public string TownName
+        {
+            get
+            {
+                return townName;
+            }
+            set
+            {
+                townName = value;
+            }
+        }
+
+        public string PostCode
+        {
+            get
+            {
+                return postCode;
+            }
+            set
+            {
+                postCode = value;
+            }
+        }
+        public string County
+        {
+            get
+            {
+                return county;
+            }
+            set
+            {
+                county = value;
+            }
+        }
+        public string ContactPhoneNo
+        {
+            get
+            {
+                return ContactPhoneNo;
+            }
+            set
+            {
+                contactPhoneNo = value;
+            }
+        }
+        public string ContactEmail
+        {
+            get
+            {
+                return contactEmail;
+            }
+            set
+            {
+                contactEmail = value;
+            }
+        }
+    }
+}
+
