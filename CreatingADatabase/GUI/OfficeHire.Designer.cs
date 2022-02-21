6b220ecb2b10d10e85aec56a965b9399dbd81cd3
Peter Stribblehill <peter.strib@gmail.com>
2022-02-21 21:52:43 +0000

    Adds UI for making a booking in the Office hire form

--- CreatingADatabase/GUI/OfficeHire.Designer.cs
+++ CreatingADatabase/GUI/OfficeHire.Designer.cs
@@ -36,18 +36,24 @@ namespace CreatingADatabase.GUI
             this.DGVAvailability = new System.Windows.Forms.DataGridView();
             this.btnBackToMenu = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.DGVAvailability)).BeginInit();
             this.SuspendLayout();
             // 
             // CBoxClients
             // 
             this.CBoxClients.FormattingEnabled = true;
-            this.CBoxClients.Location = new System.Drawing.Point(19, 31);
+            this.CBoxClients.Location = new System.Drawing.Point(383, 157);
             this.CBoxClients.Name = "CBoxClients";
-            this.CBoxClients.Size = new System.Drawing.Size(314, 21);
+            this.CBoxClients.Size = new System.Drawing.Size(201, 21);
             this.CBoxClients.TabIndex = 2;
             // 
             // CBoxYear
@@ -68,7 +74,7 @@ namespace CreatingADatabase.GUI
             "2033",
             "2034",
             "2035"});
-            this.CBoxYear.Location = new System.Drawing.Point(161, 72);
+            this.CBoxYear.Location = new System.Drawing.Point(19, 63);
             this.CBoxYear.Name = "CBoxYear";
             this.CBoxYear.Size = new System.Drawing.Size(120, 21);
             this.CBoxYear.TabIndex = 3;
@@ -76,16 +82,17 @@ namespace CreatingADatabase.GUI
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 138);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Client:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(162, 57);
+            this.label2.Location = new System.Drawing.Point(20, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 5;
@@ -96,17 +103,17 @@ namespace CreatingADatabase.GUI
             this.DGVAvailability.AllowUserToAddRows = false;
             this.DGVAvailability.AllowUserToDeleteRows = false;
             this.DGVAvailability.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGVAvailability.Location = new System.Drawing.Point(20, 120);
+            this.DGVAvailability.Location = new System.Drawing.Point(19, 109);
             this.DGVAvailability.Name = "DGVAvailability";
             this.DGVAvailability.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.DGVAvailability.Size = new System.Drawing.Size(355, 328);
+            this.DGVAvailability.Size = new System.Drawing.Size(355, 293);
             this.DGVAvailability.TabIndex = 6;
             this.DGVAvailability.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVAvailabilityCellClick);
             // 
             // btnBackToMenu
             // 
             this.btnBackToMenu.Font = new System.Drawing.Font("Bahnschrift", 10F);
-            this.btnBackToMenu.Location = new System.Drawing.Point(169, 1);
+            this.btnBackToMenu.Location = new System.Drawing.Point(12, 10);
             this.btnBackToMenu.Name = "btnBackToMenu";
             this.btnBackToMenu.Size = new System.Drawing.Size(53, 26);
             this.btnBackToMenu.TabIndex = 7;
@@ -116,7 +123,8 @@ namespace CreatingADatabase.GUI
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(295, 72);
+            this.btnSearch.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(152, 62);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 8;
@@ -124,41 +132,101 @@ namespace CreatingADatabase.GUI
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 58);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Month:";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(107, 104);
+            this.label4.Location = new System.Drawing.Point(106, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Room Number:";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(383, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Refresh Table";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(380, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Make a new booking:";
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 77);
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(380, 181);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(143, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "The month is always January";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Start Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(380, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "End Date:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(383, 288);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(201, 21);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(380, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Room No.";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(383, 202);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(201, 20);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(383, 245);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(201, 20);
+            this.dateTimePicker2.TabIndex = 21;
             // 
             // OfficeHire
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 464);
+            this.ClientSize = new System.Drawing.Size(612, 417);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnBackToMenu);
             this.Controls.Add(this.DGVAvailability);
@@ -184,8 +252,14 @@ namespace CreatingADatabase.GUI
         private System.Windows.Forms.DataGridView DGVAvailability;
         private System.Windows.Forms.Button btnBackToMenu;
         private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
